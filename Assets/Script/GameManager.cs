622d5db8efcb9b085995042e8590c3a22c127f38
dongho5309 <dongho5309@naver.com>
2019-10-11 10:57:22 +0900

    초기 커밋

---
+++ Assets/Script/GameManager.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    private static GameManager _instance = null;
+    // Start is called before the first frame update
+    public static GameManager Instance
+    {
+        get
+        {
+            if (_instance == null)
+            {
+                _instance = FindObjectOfType(typeof(GameManager)) as GameManager;
+                if (_instance == null)
+                {
+                    Debug.Log("There is no AppManager");
+                }
+            }
+            return _instance;
+        }
+    }
+    public bool gameStart = false;
+
+}
---
+++ Assets/Script/GameStart.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameStart : MonoBehaviour
+{
+    public Text resultText;
+    int startIn5sec = 5;
+    // Start is called before the first frame update
+    void Start()
+    {
+        if(GameManager.Instance.gameStart == false)
+        {
+            resultText.text = "Ready For Battle";
+            InvokeRepeating("Count5sec", 3f, 1f);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Debug.Log(resultText.text);
+    }
+
+    void Count5sec()
+    {
+        if(startIn5sec == -1)
+        {
+            CancelInvoke("Count5sec");
+            GameManager.Instance.gameStart = true;
+            resultText.text = "Fight!";
+        }
+        else
+        {
+            resultText.text = "Start Game In " + startIn5sec + "sec";
+            startIn5sec--;
+        }
+    }
+}
