abc3b47f4049a4a27c5c2b6c22c339f3dd0a88e0
dongho5309 <dongho5309@naver.com>
2019-10-16 18:26:13 +0900

    자동전투 일부 구현(캐릭터가 상대편을 바라보고 쫓아가게 한다) 공격부분 아직 미완성 hp가 다 떨어지거나 시간이 60초가 지나면 게임이 끝나는 것까지 정리 그외 코드 함수들에 주석 작성 애니메이터 트리가 현재 안 보여서 다시 임포트해서 애니메이션트리 정돈 필요

---
+++ Assets/Auto_fight_Script.cs
@@ -0,0 +1,83 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Auto_fight_Script : MonoBehaviour
+{
+    public GameObject FightTarget;
+    public float moveSpeed = 3f;//움직이는 속도로 디버그용으로 우선 public 선언
+    CharacterController characterController;
+    Animator characterAnimator;
+    private bool atkInRange = false;
+    enum BotType {knight, undead};
+    BotType botType;
+    // Start is called before the first frame update
+    void Start()
+    {
+        characterController = GetComponent<CharacterController>();
+        characterAnimator = GetComponent<Animator>();
+        StartCoroutine(CharacterAttack());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (GameManager.Instance.gameStart && !GameManager.Instance.gameEnded)
+        {
+            if (!atkInRange)
+            {
+                CharacterMove();
+            }
+            else
+            {
+                CharacterAttack();
+            }
+        }
+        else if (GameManager.Instance.gameEnded)
+        {
+            characterAnimator.SetInteger("anim_Status", 0);
+        }
+    }
+
+    /// <summary>
+    /// 항상 상대하는 적을 바라보고 움직입니다.
+    /// </summary>
+    void CharacterMove()
+    {
+        transform.LookAt(FightTarget.transform);
+        characterAnimator.SetInteger("anim_Status", 1);
+        characterController.Move(moveSpeed * Time.deltaTime * transform.forward);
+    }
+    /// <summary>
+    /// 적을 공격합니다.
+    /// </summary>
+    IEnumerator CharacterAttack()
+    {
+        characterAnimator.SetInteger("anim_Status", 2);
+        if(botType == BotType.knight)
+        {
+            GameManager.Instance.undeadHP -= 20f;
+        }
+        else if(botType == BotType.undead)
+        {
+            GameManager.Instance.knightHP -= 20f;
+        }
+        characterAnimator.SetInteger("anim_Status", 0);
+        yield return new WaitForSeconds(1.5f);
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if(collision.transform == FightTarget.transform)
+        {
+            atkInRange = true;
+        }
+    }
+    private void OnCollisionExit(Collision collision)
+    {
+        if (collision.transform == FightTarget.transform)
+        {
+            atkInRange = false;
+        }
+    }
+}
--- Assets/Script/GameManager.cs
+++ Assets/Script/GameManager.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class GameManager : MonoBehaviour
 {
@@ -24,8 +25,32 @@ public class GameManager : MonoBehaviour
     [Header("시작,끝boolean")]
     public bool gameStart = false;
     public bool gameEnded = false;
+    [Header("Bot HP")]
+    public float knightHP, undeadHP = 100f;
+    [Header("HPBar")]
+    public Image knightBar, undeadBar;
     void Update()
     {
-        
+        HPBar_Visual();
+
+    }
+
+    /// <summary>
+    /// knight 와 undead의 HP bar(Image)의 변화를 가시화해주는 함수입니다.
+    /// </summary>
+    void HPBar_Visual()
+    {
+        knightBar.fillAmount = knightHP / 100;
+        undeadBar.fillAmount = undeadHP / 100;
+    }
+    /// <summary>
+    /// knight와 undead 중 한쪽의 피가 다 닳으면 게임을 즉시 종료시키는 gameEnded를 true로 변환
+    /// </summary>
+    void HpZero()
+    {
+        if(knightHP <= 0 || undeadHP <= 0)
+        {
+            gameEnded = true;
+        }
     }
 }
