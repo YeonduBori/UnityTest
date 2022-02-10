a112579d95f8e73ca82690aa20780e9e82429657
DonghoShin <dongho5309@naver.com>
2022-02-11 00:02:10 +0900

    UniRx Difference Between Select SelectMany

---
+++ UniRxPractice/Assets/CorutineTest.cs
@@ -0,0 +1,48 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UniRx;
+using System;
+
+public class CorutineTest : MonoBehaviour
+{
+    IDisposable cancelEvent;
+    // Start is called before the first frame update
+    void Start()
+    {
+        //cancelEvent = Observable.FromCoroutine(AsyncA).
+        //                  SelectMany(AsyncB).
+        //                  Select(AsyncC).
+        //                  Subscribe();
+    }
+
+    private void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            Debug.Log("SpaceBar Dispose!");
+            cancelEvent.Dispose();
+        }
+    }
+
+    IEnumerator AsyncA()
+    {
+        Debug.Log("a start");
+        yield return new WaitForSeconds(1);
+        Debug.Log("a end");
+    }
+
+    IEnumerator AsyncB()
+    {
+        Debug.Log("b start");
+        yield return new WaitForSeconds(3);
+        Debug.Log("b end");
+    }
+
+    IEnumerator AsyncC()
+    {
+        Debug.Log("c start");
+        yield return new WaitForSeconds(5);
+        Debug.Log("c end");
+    }
+}
